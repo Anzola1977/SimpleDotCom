dbd74deb249c4a06662068ee0cacf3e95dcc4735
Andrei Zola <anzola@yandex.ru>
2021-11-14 14:58:13 +0100

    Next commit

--- src/SimpleDotCom.java
+++ src/SimpleDotCom.java
@@ -2,11 +2,24 @@ public class SimpleDotCom {
     int[] locationCells;
     int numOfHits = 0;
 
-    String checkYourself(String userGuess) {
-
+    public String checkYourself(String stringGuess) {
+        int guess = Integer.parseInt(stringGuess);
+        String result = "Мимо";
+        for (int cell : locationCells) {
+            if (guess == cell) {
+                result = "Попал";
+                numOfHits++;
+                break;
+            }
+        }
+        if (numOfHits == locationCells.length) {
+            result = "Потопил";
+        }
+        System.out.println(result);
+            return result;
     }
 
-    void setLocationCells(int[] locationCells) {
-
+    public void setLocationCells(int[] locs) {
+        locationCells = locs;
     }
 }
